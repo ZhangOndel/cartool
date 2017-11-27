227bdc3c0d4071c7461de5b6a2267052741ea41a
jinxiao <jinxiao@codoon.com>
2017-11-27 21:07:54 +0800

    supports 2x 3x

--- cartool/main.m
+++ cartool/main.m
@@ -40,16 +40,11 @@
 @end
 
 @interface CUICatalog : NSObject
-
-@property(readonly) bool isVectorBased;
-
 -(id)initWithName:(NSString *)n fromBundle:(NSBundle *)b;
 -(id)allKeys;
--(id)allImageNames;
 -(CUINamedImage *)imageWithName:(NSString *)n scaleFactor:(CGFloat)s;
 -(CUINamedImage *)imageWithName:(NSString *)n scaleFactor:(CGFloat)s deviceIdiom:(int)idiom;
 -(NSArray *)imagesWithName:(NSString *)n;
-
 @end
 
 #define kCoreThemeIdiomPhone 1
@@ -60,76 +55,59 @@ void CGImageWriteToFile(CGImageRef image, NSString *path)
     CFURLRef url = (__bridge CFURLRef)[NSURL fileURLWithPath:path];
     CGImageDestinationRef destination = CGImageDestinationCreateWithURL(url, kUTTypePNG, 1, NULL);
     CGImageDestinationAddImage(destination, image, nil);
-	
+    
     if (!CGImageDestinationFinalize(destination)) {
         NSLog(@"Failed to write image to %@", path);
     }
-	
+    
     CFRelease(destination);
 }
 
 
 void exportCarFileAtPath(NSString * carPath, NSString *outputDirectoryPath)
 {
-	NSError *error = nil;
-	
-	outputDirectoryPath = [outputDirectoryPath stringByExpandingTildeInPath];
-
-	CUIThemeFacet *facet = [CUIThemeFacet themeWithContentsOfURL:[NSURL fileURLWithPath:carPath] error:&error];
-	
-	CUICatalog *catalog = [[CUICatalog alloc] init];
-	
-	/* Override CUICatalog to point to a file rather than a bundle */
-	[catalog setValue:facet forKey:@"_storageRef"];
-	
-	/* CUICommonAssetStorage won't link */
-	CUICommonAssetStorage *storage = [[NSClassFromString(@"CUICommonAssetStorage") alloc] initWithPath:carPath];
-	
-	for (NSString *key in [storage allRenditionNames])
-	{
-		printf("%s\n", [key UTF8String]);
-
-        NSArray *images = [catalog imagesWithName:key];
-        for( CUINamedImage *image in images )
-        {
-            if( CGSizeEqualToSize(image.size, CGSizeZero) )
-                printf("\tappears to have a PDF implementation\n");
-            else
-            {
-                CGImageRef cgImage = [image image];
-                NSString *idiom = @"";
-                switch( image.idiom )
-                {
-                    case kCoreThemeIdiomPhone:
-                        idiom = @"~iphone";
-                        break;
-                    case kCoreThemeIdiomPad:
-                        idiom = @"~ipad";
-                        break;
-                    default:
-                        break;
-                }
-                NSString *scale = image.scale > 1.0 ? [NSString stringWithFormat:@"@%dx", (int)floor(image.scale)] : @"";
-                NSString *name = [NSString stringWithFormat:@"%@%@%@.png", key, idiom, scale];
-                printf("\t%s\n", [name UTF8String]);
-                if( outputDirectoryPath )
-                    CGImageWriteToFile(cgImage, [outputDirectoryPath stringByAppendingPathComponent:name]);
+    NSError *error = nil;
+    
+    outputDirectoryPath = [outputDirectoryPath stringByExpandingTildeInPath];
+    
+    CUIThemeFacet *facet = [CUIThemeFacet themeWithContentsOfURL:[NSURL fileURLWithPath:carPath] error:&error];
+    
+    CUICatalog *catalog = [[CUICatalog alloc] init];
+    
+    [catalog setValue:facet forKey:@"_storageRef"];
+    
+    CUICommonAssetStorage *storage = [[NSClassFromString(@"CUICommonAssetStorage") alloc] initWithPath:carPath];
+    
+    for (NSString *key in [storage allRenditionNames])
+    {
+        printf("%s\n", [key UTF8String]);
+        
+        for(NSString *scale in @[@"3", @"2"]) {
+            CGImageRef imageRef = [[catalog imageWithName:key scaleFactor:scale.doubleValue] image];
+            
+            if(imageRef == nil) {
+                continue;
+                
             }
+            CGFloat width = CGImageGetWidth(imageRef);
+            CGImageWriteToFile(imageRef, [outputDirectoryPath stringByAppendingPathComponent:[NSString stringWithFormat:@"%@@%@x.png", key, scale]]);
         }
-	}
+    }
 }
 
 int main(int argc, const char * argv[])
 {
-	@autoreleasepool {
-	    
-		if (argc < 2)
-		{
-			printf("Usage: cartool <oath to Assets.car> [outputDirectory]\n");
-			return -1;
-		}
-	    
-		exportCarFileAtPath([NSString stringWithUTF8String:argv[1]], argc > 2 ? [NSString stringWithUTF8String:argv[2]] : nil);
-	}
+    @autoreleasepool {
+        
+        if (argc != 3)
+        {
+            printf("Usage: cartool Assets.car outputDirectory\n");
+            return -1;
+        }
+        
+        exportCarFileAtPath([NSString stringWithUTF8String:argv[1]], [NSString stringWithUTF8String:argv[2]]);
+        
+    }
     return 0;
 }
+
